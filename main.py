204a1901256158ef86babf81989a463d341b93d6
anton-plvk <anton-plvk@users.noreply.github.com>
2026-02-01 23:31:03 +0200

    added output parsing

--- main.py
+++ main.py
@@ -1,38 +1,49 @@
 from dotenv import load_dotenv
 
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
+
 load_dotenv()
 
 from langchain_classic import hub
-
 from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
 tools = [TavilySearch()]
-llm = ChatOpenAI(model='gpt-4')
-react_prompt = hub.pull("hwchase17/react")
+llm = ChatOpenAI(model="gpt-4")
+# react_prompt = hub.pull("hwchase17/react")
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tools", "tool_names"],
+).partial(format_instructions=output_parser.get_format_instructions())
 agent = create_react_agent(
-    llm=llm,
-    tools=tools,
-    prompt=react_prompt
+    llm=llm, tools=tools, prompt=react_prompt_with_format_instructions
 )
 agent_executor = AgentExecutor(
-    agent=agent,
-    tools=tools,
-    verbose=True)
+    agent=agent, tools=tools, verbose=True, handle_parsing_errors=True
+)
 
-chain = agent_executor
+extract_output = RunnableLambda(lambda x: x["output"])
+parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+chain = agent_executor | extract_output | parse_output
+
 
 def main():
     print("Hello from langchain-course!")
     result = chain.invoke(
         input={
-            "input": "search for 3 job postinig for an ai engineer with langchain as one of the skillsets in the bay area on linkdin and list a short summary of their details. Also make sure that it is open for hire. Preferably posted in the last week."
+            "input": "search for 3 job postinig for an ai engineer with langchain as one of the skillsets in the bay area on linkdin and list a short summary of their details."
         }
     )
     print(result)
-    print('done')
+    print("done")
+
 
 if __name__ == "__main__":
     main()
