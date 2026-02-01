dbbea02bf0162236d480092841d48f23b0efc9d0
anton-plvk <anton-plvk@users.noreply.github.com>
2026-02-02 00:10:42 +0200

    with structured output

--- main.py
+++ main.py
@@ -16,22 +16,23 @@ from langchain_tavily import TavilySearch
 
 tools = [TavilySearch()]
 llm = ChatOpenAI(model="gpt-4")
-# react_prompt = hub.pull("hwchase17/react")
-output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
-react_prompt_with_format_instructions = PromptTemplate(
-    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
-    input_variables=["input", "agent_scratchpad", "tools", "tool_names"],
-).partial(format_instructions=output_parser.get_format_instructions())
+react_prompt = hub.pull("hwchase17/react")
+# output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+structured_llm = llm.with_structured_output(AgentResponse)
+# react_prompt_with_format_instructions = PromptTemplate(
+#     template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+#     input_variables=["input", "agent_scratchpad", "tools", "tool_names"],
+# ).partial(format_instructions='')
 agent = create_react_agent(
-    llm=llm, tools=tools, prompt=react_prompt_with_format_instructions
+    llm=llm, tools=tools, prompt=react_prompt
 )
 agent_executor = AgentExecutor(
     agent=agent, tools=tools, verbose=True, handle_parsing_errors=True
 )
 
 extract_output = RunnableLambda(lambda x: x["output"])
-parse_output = RunnableLambda(lambda x: output_parser.parse(x))
-chain = agent_executor | extract_output | parse_output
+# parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+chain = agent_executor | extract_output | structured_llm
 
 
 def main():
